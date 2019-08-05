d2f18483ae2ccb144be0a1e4a5308fed97866c2c
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-08-05 23:57:57 +0900

    Update: main func

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"github.com/ciruclation-dev/gotrading/bitflyer"
 	"github.com/ciruclation-dev/gotrading/confing"
 	"github.com/ciruclation-dev/gotrading/utils"
-	"log"
 )
 
 func main() {
 	utils.LoggingSettings(confing.Config.LogFile)
-	log.Println("test")
+	apiClient := bitflyer.New(confing.Config.ApiKey, confing.Config.ApiSecret)
+	fmt.Println(apiClient.GetBalance())
 }
