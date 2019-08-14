ea78b417a43eae2a7e72e8ee9a1e6a685bca226c
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-08-14 18:20:58 +0900

    Update new function in this model

--- app/models/candle.go
+++ app/models/candle.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"github.com/ciruclation-dev/gotrading/bitflyer"
+	"time"
+)
 
 type Candle struct {
 	ProductCode string
@@ -29,6 +33,50 @@ func NewCandle(productCode string, duration time.Duration, timeDate time.Time, o
 func (c *Candle) TableName() string {
 	return GetCandleTableName(c.ProductCode, c.Duration)
 }
-func ()  {
-	
+func (c *Candle) Create() error {
+	cmd := fmt.Sprintf("INSERT INTO %s (time, open, high, low, volume) VALUES (?, ? , ? , ?)", c.TableName())
+	_, err := Dbconnection.Exec(cmd, c.Time.Format(time.RFC3339), c.Open, c.Close, c.High, c.Low, c.Volume)
+	if err != nil {
+		return err
+	}
+}
+
+func (c *Candle) Save() error {
+	cmd := fmt.Sprintf("UPDATE %s SET open = ?, high = ?, low = ?, volume = ? WHERE time = ?", c.TableName())
+	_, err := Dbconnection.Exec(cmd, c.Open, c.Close, c.High, c.Low, c.Volume, c.Time.Format(time.RFC3339))
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func GetCandle(productCode string, duration time.Duration, datetime time.Time) *Candle {
+	tableName := GetCandleTableName(productCode, duration)
+	cmd := fmt.Sprintf("SELECT time, open, close, high, low, volume FROM %s WHERE time = ?", tableName)
+	row := Dbconnection.QueryRow(cmd, datetime.Format(time.RFC3339))
+	var candle Candle
+	err := row.Scan(&candle.Time, &candle.Open, &candle.Close, &candle.High, &candle.Low, &candle.Volume)
+	if err != nil {
+		return nil
+	}
+	return NewCandle(productCode, duration, candle.Time, candle.Open, candle.Close, candle.High, candle.Low, candle.Volume)
+}
+
+func CreateCandleWithDuration(ticker bitflyer.Ticker, productCode string, duration time.Duration) bool {
+	currentCandle := GetCandle(productCode, duration, ticker.TruncateDateTime(duration))
+	price := ticker.GetMidPrice()
+	if currentCandle == nil {
+		candle := NewCandle(productCode, duration, ticker.TruncateDateTime(duration), price, price, price, price, ticker.Volume)
+		candle.Create()
+		return true
+	}
+	if currentCandle.High <= price {
+		currentCandle.High = price
+	} else if currentCandle.Low >= price {
+		currentCandle.Low = price
+	}
+	currentCandle.Volume += ticker.Volume
+	currentCandle.Close = price
+	currentCandle.Save()
+	return false
 }
