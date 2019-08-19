f2a10b976f64eddc70d6a546029f9d873afbb7d8
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-08-19 22:46:28 +0900

    Refactor filename, fix import name

--- app/controllers/streamdata.go
+++ app/controllers/streamdata.go
@@ -11,13 +11,16 @@ func StreamIngestionData() {
 	var tickerChannl = make(chan bitflyer.Ticker)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
 	go apiClient.GetRealTimeTicker(config.Config.Productcode, tickerChannl)
-	for ticker := range tickerChannl {
-		log.Println("action=StreamIngestionData, %v", ticker)
-		for _, duration := range config.Config.Durations {
-			isCreated := models.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
-			if isCreated == true && duration == config.Config.TradeDuration {
-				// Todo
+	go func() {
+		for ticker := range tickerChannl {
+			log.Println("action=StreamIngestionData, %v", ticker)
+			for _, duration := range config.Config.Durations {
+				isCreated := models.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
+				if isCreated == true && duration == config.Config.TradeDuration {
+					// Todo
+				}
 			}
 		}
-	}
+	}()
+
 }
