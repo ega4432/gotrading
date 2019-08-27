4ca3ef2850953f0d285fa82664bfc9c65e99c226
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-08-27 23:23:21 +0900

    Add handler, json type

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -82,6 +82,7 @@ func apiCandleHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func StartWebServer() error {
+	http.HandleFunc("/api/candle", apiMakeHandler(apiCandleHandler))
 	http.HandleFunc("/chart/", viewChartHandler)
 	return http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil)
 }
