37688c61701198f1e54926a2b984ba861bb4d625
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-09-13 00:20:39 +0900

    Delete unuse handler

--- main.go
+++ main.go
@@ -2,20 +2,12 @@ package main
 
 import (
 	"github.com/ciruclation-dev/gotrading/app/controllers"
-	//"github.com/ciruclation-dev/gotrading/bitflyer"
 	"github.com/ciruclation-dev/gotrading/config"
 	"github.com/ciruclation-dev/gotrading/utils"
-	//"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	//apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-	//ticker, _ := apiClient.GetTicker("BTC_USD")
-	//fmt.Println(ticker)
-	//fmt.Println(ticker.GetMidPrice())
-	//fmt.Println(ticker.DateTime())
-	//fmt.Println(ticker.TruncateDateTime(time.Hour))
 	controllers.StreamIngestionData()
 	controllers.StartWebServer()
 }
