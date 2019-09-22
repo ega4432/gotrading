e37d6d97ed669285d3f50745698d9d74eb894f9c
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-09-22 14:38:18 +0900

    Create view of events

--- main.go
+++ main.go
@@ -1,31 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"github.com/ciruclation-dev/gotrading/app/models"
-	"time"
+	"github.com/ciruclation-dev/gotrading/app/controllers"
+	"github.com/ciruclation-dev/gotrading/config"
+	"github.com/ciruclation-dev/gotrading/utils"
+	"log"
 )
 
-//import (
-//	"github.com/ciruclation-dev/gotrading/app/controllers"
-//	"github.com/ciruclation-dev/gotrading/config"
-//	"github.com/ciruclation-dev/gotrading/utils"
-//)
-//
-//func main() {
-//	utils.LoggingSettings(config.Config.LogFile)
-//	controllers.StreamIngestionData()
-//	controllers.StartWebServer()
-//}
-
 func main() {
-	s := models.NewSignalEvents()
-	df, _ := models.GetAllCandle("BTC_USD", time.Minute, 10)
-	c1 := df.Candles[0]
-	c2 := df.Candles[5]
-	s.Buy("BTC_USD", c1.Time.UTC(), c1.Close, 1.0, true)
-	s.Sell("BTC_USD", c2.Time.UTC(), c2.Close, 1.0, true)
-	fmt.Println(models.GetSignalEventsByCount(1))
-	fmt.Println(models.GetSignalEventsAfterTime(time.Now().UTC()))
-	fmt.Println(models.GetSignalEventsAfterTime(c1.Time))
+	utils.LoggingSettings(config.Config.LogFile)
+	controllers.StreamIngestionData()
+	log.Println(controllers.StartWebServer())
 }
